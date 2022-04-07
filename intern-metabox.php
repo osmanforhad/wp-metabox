d7c18770e3d5a3a5af93846039a1383a441e476e
Osman Forhad <osmanforhad93@gmail.com>
2022-04-07 10:34:16 +0600

    project setup

---
+++ intern-metabox.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name:       MetaBox Plugin
+ * Plugin URI:        https://osmanforhad.net/plugins/practice/
+ * Description:       CBX WordPress Plugin Development Intern MetaBox.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      8.1
+ * Author:            osman forhad
+ * Author URI:        https://author.osmanforhad.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-practice-plugin
+ * Domain Path:       /languages
+ */
